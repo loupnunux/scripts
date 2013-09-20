8fe0f92f3e671a883b487f2bf1e25caa23dc9948
loupnunux <loupnunux@gmail.com>
2013-09-20 22:29:02 +0200

    LINUX : ajout de fonctions

--- linux-postinstall/1-antergos.sh
+++ linux-postinstall/1-antergos.sh
@@ -3,5 +3,22 @@
 source ./lib_arch
 
 
-arch_modules "virtualbox" "vboxdrv\nvboxnetadp\nvboxnetflt"
+
+
+# Pour virtualbox
+arch_modules "virtualbox" "vboxdrv"
+arch_modules "virtualbox" "vboxnetadp"
+arch_modules "virtualbox" "vboxnetflt"
+arch_service "dkms.service"
+
+
+# Pour cron
+arch_service cronie
+
+
+# Pour teamview
+arch_service "teamviewerd.service"
+
+
+
 
