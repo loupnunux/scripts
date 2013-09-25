016bd108e7388a1ec7a3b89d067efd1185013552
loupnunux <loup.nunux@gmail.com>
2013-09-25 22:47:27 +0200

    LINUX : correction script

--- linux-postinstall/1-antergos.sh
+++ linux-postinstall/1-antergos.sh
@@ -12,12 +12,20 @@ arch_modules "virtualbox" "vboxnetflt"
 arch_service "dkms.service"
 
 
+# Pour ssh
+arch_service sshd
+
+
 # Pour cron
 arch_service cronie
 
 
 # Pour teamview
 arch_service "teamviewerd.service"
+
+
+
+
 
 arch_testpck "toto titi git firefox"
 
