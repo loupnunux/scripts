613e2c3649db09ad554c5a27390e4b97035e7566
loupnunux <loup.nunux@gmail.com>
2013-11-12 20:28:42 +0100

    SCRIPT : ajout d'un script pour unison

---
+++ synchro.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+
+
+######################
+###          VARIABLES
+OPTIONS=$*
+REP_SCR=$(dirname $0)
+FILE_SCR=$(basename $0)
+APPS="unison"
+RUN_SYNC="/usr/bin/unison"
+
+
+
+
+######################
+###            SOURCES
+source $REP_SCR/librairie.lib
+
+
+
+
+
+######################
+###          FONCTIONS
+f_actions() {
+  _test_nb_args "1" "$OPTIONS"
+  term "Test process pour $APPS" '[ -z $(pgrep -u $USER $APPS) ]'
+
+  $RUN_SYNC $OPTIONS
+}
+
+
+
+
+
+######################
+###             SCRIPT
+f_actions
+
+exit 1
+
