c1b0f60beee7636d9df2e1687164a6e944deded8
loupnunux <loupnunux@gmail.com>
2013-11-16 15:40:00 +0100

    SYNCHRO : corrections

--- synchro.sh
+++ synchro.sh
@@ -5,12 +5,11 @@
 ######################
 ###          VARIABLES
 OPTIONS=$*
+NB_OPTIONS=$#
 REP_SCR=$(dirname $0)
 FILE_SCR=$(basename $0)
 APPS="unison"
 RUN_SYNC="/usr/bin/unison"
-
-
 
 
 ######################
@@ -24,7 +23,8 @@ source $REP_SCR/librairie.lib
 ######################
 ###          FONCTIONS
 f_actions() {
-  _test_nb_args "1" "$OPTIONS"
+  term "Nombre d'arguments (doit etre a 1) : $NB_OPTIONS" '[ $NB_OPTIONS == 1 ]'
+  term "Test du fichier de conf" '[ -e "/home/$USER/.unison/$OPTIONS" ]'
   term "Test process pour $APPS" '[ -z $(pgrep -u $USER $APPS) ]'
 
   warn "Tache de synchronisation" "$RUN_SYNC $OPTIONS"
